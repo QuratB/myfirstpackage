c4cb456fb3d646ff0c4ae5ad4f966f1ee8ee6435
Ahmad <qad300@vu.local>
2018-01-26 11:47:36 +0100

    commit

--- R/test.R
+++ R/test.R
@@ -1,3 +1,16 @@
+#'
+#'@title Square area
+#'@description Function which calculates the area of a Square
+#'@param length the lenngth of the sqaue
+#'@param width the width of the square
+#'@author Quratulain Ahmad
+#'@export
+#'
+#'
+
+
+
+
 area_of_square <- function(length,width){
   area <- length*width
   return(area)
